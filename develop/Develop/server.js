6afdb9a939ef173e695b1ecfae7aa2eb447d497d
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-22 18:47:16 -0800

    adding requirements and setting up express app

---
+++ develop/Develop/server.js
@@ -0,0 +1,23 @@
+// Setting up requirements
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
+const routes = require("./routes/index");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+const cors = require("cors");
+const AppRoutes = require("./routes/index");
+
+// Calling express and dedicating a port for it
+const app = express();
+const PORT = process.env.PORT || 3066;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use(AppRoutes);
+
+
+
