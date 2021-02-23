ed1eddf9ed557112e17b36316d868c9abb86562c
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-22 19:24:18 -0800

    creating api routes under the api.js in routes

---
+++ develop/Develop/routes/api.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+
--- develop/Develop/routes/index.js
+++ develop/Develop/routes/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
 const path = require("path");
 const fs = require("fs");
-const uuid = require("uuid");
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../db/db.json"));
@@ -13,14 +11,6 @@ router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
 });
 
-router.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("Develop/db/db.json))
-    const newNotes = req.body;
-    newNotes.id = uuid.v4();
-    notes.push(newNotes);
-    fs.writeFileSync("Develop/db/db.json", JSON.stringify(notes))
-    res.json(notes);
 
-});
 
 module.exports = router
