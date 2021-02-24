4d35e4abcfc9154b7027c3d21c2dd07822081984
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-23 20:02:33 -0800

    finished routes

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # note_taking
+
+## Table of Contents
+
+1. [Developers Comment](#Developer-Comment)
+1. [User Story](#User-Story)
+1. [Acceptance Criteria](#Acceptance-Criteria)
+1. [Languages](#Languages)
+1. [Demo](#Demo)
+1. [Links](#Links)
+1. [Installation](#Installation)
+
+## Developer Comment
+
+## User Story
+
+```
+AS A user, I want to be able to write and save notes
+
+I WANT to be able to delete notes I've written before
+
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+```
+
+## Acceptane Criteria
+
+```
+Application should allow users to create and save notes.
+
+Application should allow users to view previously saved notes.
+
+Application should allow users to delete previously saved notes.
+```
+
+## Languages
+
+
+
+## Demo
+
+## Links
+
+
+
+## Installation
+
+
---
+++ assets/Develop/server.js
@@ -0,0 +1,58 @@
+// Middleware
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3066;
+
+app.use(express.static(__dirname + '/assets/Develop/public'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let notelength = noteList.length.toString();
+
+    newNote.id = notelength;
+    noteList.push(newNote);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let noteId = req.params.id.toString();
+
+    noteList = noteList.filter((selected) => {
+        return selected.id != noteId;
+    });
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
+
