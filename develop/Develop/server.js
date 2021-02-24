4aa0d20bad2e1bf80c8bae0ba854f48e0b33542d
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-23 18:45:30 -0800

    got rid of routes, because I couldn't get it to post

--- develop/Develop/server.js
+++ develop/Develop/server.js
@@ -1,28 +1,21 @@
 // Setting up requirements
 const express = require("express");
 const fs = require("fs");
-const notes = require("./db/db.json");
 const path = require("path");
-// const routes = require("./routes/index");
-const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
-// const cors = require("cors");
-const AppRoutes = require("./routes/index");
+
 
 // Calling express and dedicating a port for it
 const app = express();
 const PORT = process.env.PORT || 3066;
 
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(express.static("Develop"));
+app.use(express.static(__dirname + "/Develop/public"));
 
 
-// app.use(express.static("develop/Develop/public", routes));
-// app.use(cors());
 
-app.use(AppRoutes);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
