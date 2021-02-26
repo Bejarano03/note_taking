e9cb60a9918a4b50160f6d5878e036dd2a30dcdf
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-25 23:52:46 -0800

    change some formatting on server.js

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.delete('Develop/api/notes/:id', (req, res) => {
 
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log('App listening on PORT: ' + PORT);
 });
 
