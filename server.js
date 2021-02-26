1bdb9c404675da86548101be5d735d47b72ac0f6
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-26 12:24:20 -0800

    fixing heroku issues

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.static(__dirname + '/Develop/public'));
 app.use(express.urlencoded({ extended: true }));
